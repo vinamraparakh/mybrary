08ee8050c9f676788b639a2f91a8877e819c5354
vinamra <vinamraparakh@gmail.com>
2022-12-13 13:30:57 +0530

    FormFields

--- routes/books.js
+++ routes/books.js
@@ -16,15 +16,32 @@ router.get('/', async (req, res) => {
 router.get('/new', async (req, res) => {
     try {
         const authors = await Author.find({});
-        res.render('books/new', { authors: author, book: new Book() });
+        res.render('books/new', { authors: authors, book: new Book() });
     } catch {
-        res.redirect('/books');
+        res.send("Error");
+        // res.redirect('/books');
     }
 });
 
 // create book route
 router.post('/', async (req, res) => {
     res.send('Create Book');
+
+    const author = new Author({
+        name: req.body.name
+    });
+
+    try {
+        const newAuthor = await author.save();
+        // res.redirect(`authors/${newAuthor.id}`);
+        res.redirect(`authors`);
+    }
+    catch {
+        res.render('authors/new', {
+            author: author,
+            errorMessage: 'Error creating Author'
+        });
+    }
 });
 
 module.exports = router;
